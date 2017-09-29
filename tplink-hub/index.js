93c037edf2e4c7cf27b0fbd8ec50de68eb40d05a
RCGCoder <ricardo.cantabrana@gmail.com>
2017-09-29 23:17:21 +0200

    test9

--- tplink-hub/index.js
+++ tplink-hub/index.js
@@ -67,16 +67,20 @@ app.get('/plugs/:deviceId/off', function (req, res) {
   }
 });
 
-const  {exec} = require('child_process');
-exec('kodi-send --host=192.168.100.11 --action="ActivateScreenSaver"', (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
 
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
+const { spawn } = require('child_process');
+const ls = spawn('kodi-send --host=192.168.100.11 --action="ActivateScreenSaver"', []);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
 
 
