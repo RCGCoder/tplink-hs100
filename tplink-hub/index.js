de79dc2cd99f34cce50754ffea014ed6e79347a1
RCGCoder <ricardo.cantabrana@gmail.com>
2017-09-29 07:42:18 +0200

    Includes Kodi jsonRPC

--- tplink-hub/index.js
+++ tplink-hub/index.js
@@ -67,6 +67,46 @@ app.get('/plugs/:deviceId/off', function (req, res) {
   }
 });
 
+app.get('/kodi/:deviceId/screensaver/on', function (req, res) {
+	logger.info('[API][Kodi][ScreenSaver][On] - turn on kodi screensaver', req.params.deviceId);
+	var xbmc = require('xbmc');
+
+	// Connect to XBMC
+	// The options below are the defaults
+	xbmc.connect({
+	  host: req.params.deviceId,
+	  port: 8080,
+	  reconnect: false,
+	  reconnect_sleep: 3000
+	});
+
+	// Lets you know when we are connected
+	xbmc.on('connect', function () {
+	  console.log("connected to xbmc!");
+	});
+
+	// Run any XBMC API command and get the results
+	// NOTE: If you are not connected the call will be placed in a queue and executed as the connection is restored.
+	xbmc.run("GUI.ActivateWindow", {"window":"screensaver"}, function (res) {
+	  console.log(res.result);
+	});
+
+	// Get notified on any notifications you desire
+	/*xbmc.on("Application.OnVolumeChanged", function (data) {
+	  console.log(data);
+	});*/
+
+	// Changes connection options
+	/*xbmc.setOptions({
+	  host: "localhost",
+	  port: 9090,
+	  reconnect: true,
+	  reconnect_sleep: 30000
+	});*/
+
+	// Closes the connection to XBMC
+	xbmc.close();
+});
 
 app.listen(3000, () => {
   logger.info('[API] - listening on port 3000');
